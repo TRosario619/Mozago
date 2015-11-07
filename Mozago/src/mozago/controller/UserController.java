0c0b4ef77d94b93729f7628a3ddda95294a107ce
hnoor <haider_noor@live.com>
2015-11-07 13:38:01 +0200

    user - > userController

--- Mozago/src/mozago/controller/UserController.java
+++ Mozago/src/mozago/controller/UserController.java
@@ -1,5 +1,5 @@
 package mozago.controller;
 
-public class User {
+public class UserController {
 
 }
--- Mozago/src/mozago/model/Despesa.java
+++ Mozago/src/mozago/model/Despesa.java
@@ -2,6 +2,36 @@ package mozago.model;
 
 public class Despesa {
 String ID, tipo, descricao;
+public String getID() {
+	return ID;
+}
+public void setID(String iD) {
+	ID = iD;
+}
+public String getTipo() {
+	return tipo;
+}
+public void setTipo(String tipo) {
+	this.tipo = tipo;
+}
+public String getDescricao() {
+	return descricao;
+}
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+public double getValor() {
+	return valor;
+}
+public void setValor(double valor) {
+	this.valor = valor;
+}
+public double getQuantidade() {
+	return quantidade;
+}
+public void setQuantidade(double quantidade) {
+	this.quantidade = quantidade;
+}
 double valor, quantidade;
 
 }
