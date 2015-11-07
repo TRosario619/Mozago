2fda0331c6e0ead4c3016070d9b8174a7b9d7d79
hnoor <haider_noor@live.com>
2015-11-07 13:50:17 +0200

    validar

--- Mozago/src/mozago/controller/UserController.java
+++ Mozago/src/mozago/controller/UserController.java
@@ -1,5 +1,17 @@
 package mozago.controller;
 
+import mozago.model.User;
+
 public class UserController {
 
+	public boolean validar(String nome,String apelido,String email,String password,String username,String tipo,
+	long telefone){
+		
+		if (nome.isEmpty() || apelido.isEmpty() || email.isEmpty() ||password.isEmpty() || username.isEmpty() ||
+				tipo.isEmpty() || telefone == 0)
+			return false;
+		else 
+			
+		return true;
+	}
 }
