731e5f67f805a2a58d75fce83d36f43d6a8d731f
hnoor <haider_noor@live.com>
2015-11-07 13:09:15 +0200

    user

---
+++ Mozago/src/mozago/model/User.java
@@ -0,0 +1,48 @@
+package mozago.model;
+
+public class User {
+	
+	String nome, apelido, email, password,username;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getApelido() {
+		return apelido;
+	}
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+
+}
