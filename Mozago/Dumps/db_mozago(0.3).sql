8dcd9a9b74e06c1a4d63c3b7f9a675ff923f0732
hnoor <haider_noor@live.com>
2015-11-10 14:27:16 +0200

    base de dados com users admin, gestor e director.

---
+++ Mozago/Dumps/db_mozago(0.3).sql
@@ -0,0 +1,256 @@
+CREATE DATABASE  IF NOT EXISTS `mozago_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `mozago_db`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: mozago_db
+-- ------------------------------------------------------
+-- Server version	5.6.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alocacao`
+--
+
+DROP TABLE IF EXISTS `alocacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alocacao` (
+  `idalocacao` int(11) NOT NULL,
+  `alocador` int(11) DEFAULT NULL,
+  `alocado` int(11) DEFAULT NULL,
+  `obra` int(11) DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  PRIMARY KEY (`idalocacao`),
+  KEY `fk_alocador_idx` (`alocador`),
+  KEY `fk_alocado_idx` (`alocado`),
+  KEY `fk_obra_idx` (`obra`),
+  CONSTRAINT `fk_alocado` FOREIGN KEY (`alocado`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_alocador` FOREIGN KEY (`alocador`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_obra` FOREIGN KEY (`obra`) REFERENCES `obra` (`idObra`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alocacao`
+--
+
+LOCK TABLES `alocacao` WRITE;
+/*!40000 ALTER TABLE `alocacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alocacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `idcategoria` int(11) NOT NULL,
+  `catnome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idcategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (1,'admin'),(2,'director'),(3,'gestor');
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `despesa`
+--
+
+DROP TABLE IF EXISTS `despesa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `despesa` (
+  `id_despesa` int(11) NOT NULL,
+  `material_id` int(11) DEFAULT NULL,
+  `quantia` int(11) DEFAULT NULL,
+  `preco_total` bigint(20) DEFAULT NULL,
+  `data_despesa` date DEFAULT NULL,
+  PRIMARY KEY (`id_despesa`),
+  KEY `fk_material_idx` (`material_id`),
+  CONSTRAINT `fk_material` FOREIGN KEY (`material_id`) REFERENCES `material` (`idmaterial`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `despesa`
+--
+
+LOCK TABLES `despesa` WRITE;
+/*!40000 ALTER TABLE `despesa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `despesa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `investimento`
+--
+
+DROP TABLE IF EXISTS `investimento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `investimento` (
+  `idinvestimento` int(11) NOT NULL,
+  `valor_invest` int(11) DEFAULT NULL,
+  `data_invest` date DEFAULT NULL,
+  `descricao_inevest` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idinvestimento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `investimento`
+--
+
+LOCK TABLES `investimento` WRITE;
+/*!40000 ALTER TABLE `investimento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `investimento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `material`
+--
+
+DROP TABLE IF EXISTS `material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `material` (
+  `idmaterial` int(11) NOT NULL,
+  `descricao_material` varchar(1000) DEFAULT NULL,
+  `preco_unitario` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`idmaterial`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material`
+--
+
+LOCK TABLES `material` WRITE;
+/*!40000 ALTER TABLE `material` DISABLE KEYS */;
+/*!40000 ALTER TABLE `material` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notificacao`
+--
+
+DROP TABLE IF EXISTS `notificacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notificacao` (
+  `idnotificacao` int(11) NOT NULL,
+  `emissor` int(11) DEFAULT NULL,
+  `destinatario` int(11) DEFAULT NULL,
+  `mensagem` varchar(1000) DEFAULT NULL,
+  `data_envio` date DEFAULT NULL,
+  `lido` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`idnotificacao`),
+  KEY `fk_emissor_idx` (`emissor`),
+  KEY `fk_destinatario_idx` (`destinatario`),
+  CONSTRAINT `fk_destinatario` FOREIGN KEY (`destinatario`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_emissor` FOREIGN KEY (`emissor`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notificacao`
+--
+
+LOCK TABLES `notificacao` WRITE;
+/*!40000 ALTER TABLE `notificacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notificacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `obra`
+--
+
+DROP TABLE IF EXISTS `obra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `obra` (
+  `idObra` int(11) NOT NULL,
+  `descricao_obra` varchar(1000) DEFAULT NULL,
+  `dono_obra` varchar(45) DEFAULT NULL,
+  `contacto_dono_obra` varchar(45) DEFAULT NULL,
+  `tipo_obra` varchar(45) DEFAULT NULL,
+  `data_inicio` date DEFAULT NULL,
+  `data_fim` date DEFAULT NULL,
+  `data_prazo` date DEFAULT NULL,
+  `Valor_projectado` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`idObra`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `obra`
+--
+
+LOCK TABLES `obra` WRITE;
+/*!40000 ALTER TABLE `obra` DISABLE KEYS */;
+/*!40000 ALTER TABLE `obra` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `apelido` varchar(45) CHARACTER SET big5 DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `telefone` varchar(45) DEFAULT NULL,
+  `categoria` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idusuario`),
+  UNIQUE KEY `userrname_UNIQUE` (`username`),
+  KEY `fk_categoria_idx` (`categoria`),
+  CONSTRAINT `fk_categoria` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`idcategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'admin','admin','admin','admin','admin','123456',1),(2,'director','director','director','director','director','123456',2),(3,'gestor','gestor','gestor','gestor','gestor','123546',3);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-10 14:25:50
